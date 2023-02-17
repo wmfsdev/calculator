f68a26b96f47addb9d3ee0782fdaf7ecc4d06bef
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2023-02-17 20:17:18 +0000

    foundations for number and operator input with corresponding display output

--- README.md
+++ README.md
@@ -10,4 +10,4 @@ I expect the calculator UI to be relatively straight forward to implement and I 
 the interface clean and uncomplicated so that I can focus on the code. I can always return to 
 the visual design at a later point.
 
-For the code I anticipate to be going back over a lot of ground from the previous project; DOM manipulation - querySelectors and eventListeners for the keys, callbackFunctions for the operations, storing data in the newly learned Object type and array methods.
+For the code I anticipate to be going back over a lot of ground from the previous Etch-a-Sketch project; DOM manipulation - querySelectors and eventListeners for the keys, callbackFunctions for the operations, storing data in the newly learned Object type and array methods.
--- script.js
+++ script.js
@@ -17,12 +17,78 @@ function divide(a, b) {
     return a / b;
 }
 
-function operate(a, b, callBack) {
-    return callBack(a, b);
-}
 
-console.log(operate(5, 6, add))
+//console.log(operate(5, 6, add))
 
 // key press
 
 
+let keyString = "";
+let nextKeyString = "";
+let operator = "";
+let keyArray = [];
+let operatorArray = [];
+
+let operatorKey = document.querySelectorAll(`.key, .equals`);
+let numKey = document.querySelectorAll(`.num-key`);
+let equalsKey = document.querySelector(`.equals`);
+
+    numKey.forEach((div) => {
+
+        div.addEventListener(`click`, function (e) {
+            if (operator === "") {
+                value = e.target.innerText;
+                keyString += + value;
+                //console.log(`"1st" ${keyString}`)
+                displayNumKey(keyString);
+            } else {      
+                value = e.target.innerText;
+                keyString += + value;
+                //console.log(`"1st" ${keyString}`)
+                displayNumKey(keyString);
+            }      
+    });
+});
+
+function displayNumKey(keyStringChoice) {
+
+    let numDisplay = document.querySelector(`.number-display`);
+    numDisplay.textContent = keyStringChoice;
+}
+
+    operatorKey.forEach((div) => {
+        div.addEventListener(`click`, function(e) {
+                operator = e.target.innerText;
+                operatorArray.push(operator);
+                //console.log(operatorArray)
+                displayOperatorKey();
+                keyString = "";
+        });
+    });
+
+
+
+
+function displayOperatorKey() {
+    let operatorDisplay = document.querySelector(`.operator-display`);
+    operatorDisplay.textContent = operator;
+}
+
+// when you click operator the value stored in keyValueString is pushed into an array
+
+equalsKey.addEventListener(`click`, function (e) {
+    
+    pushKeyArray();
+  
+});
+
+function pushKeyArray() {
+
+  keyArray.push(keyValueString);
+  console.log(keyArray);
+    
+}
+
+function operate(a, b, callBack) {
+    return callBack(a, b);
+}
