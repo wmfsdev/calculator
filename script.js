7b429f4e4dfdd98f9c4b15925574fbaa4a97b5bd
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2023-02-20 00:08:36 +0000

    basic input/output functionality working as intended. tweaks left include use of single decimal point only.

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
-// basic functions
+// Object with basic functions
 
 const operatorObj = {
 
     '+': function add(a, b) { return a + b; },
     '-': function sub(a, b) { return a - b; },
-    'X': function multiply(a, b) { return a * b; },
-    '/': function divide(a, b) { return a / b; }    
+    'x': function multiply(a, b) { return a * b; },
+    '÷': function divide(a, b) { return a / b; }    
 }
 
-// --------
+// variables --------
 
 let keyString = "";
 let operator = "";
 let keyArray = [];
 let operatorArray = [];
 let display = "";
+let runningTotal = 0;
+
+
+// queries ---------
 
 let operatorKey = document.querySelectorAll(`.key`);
 let numKey = document.querySelectorAll(`.num-key`);
@@ -22,54 +26,73 @@ let equalsKey = document.querySelector(`.equals`);
 
 
 numKey.forEach((div) => {
-
     div.addEventListener(`click`, function (e) {
+        
         value = e.target.innerText;
+        console.log(typeof e.target.innerText)
         display = e.target.innerText;
-        keyString += + value;
-        // console.log(`"1st" ${keyString}`)
-        // console.log(display)
+        keyString += value;
+        console.log("num: " + keyString + " string")
+        console.log("num: " + keyArray + " array")
         topDisplay();
-        displayNumKey(keyString); 
+        displayNumKey(); 
     });
 });
 
    
 operatorKey.forEach((div) => {
     div.addEventListener(`click`, function(e) {
-        if (display === "+" || display === "" || display === "/" || display === "x" || display === "<") { 
+        if (display === "+" || display === "" || display === "÷" || display === "x" || display === "<") { 
             display = ""
             return
         } else
-        
+        console.log("--------------")
+        // console.log("operator: " + runningTotal + " running total") // 0
+        console.log(operator)                                      
+        pushKeyArray();
+        currentResult(runningTotal); 
         display = e.target.innerText
         operator = e.target.innerText
-        //console.log(keyArray)
+        console.log(keyArray)
         
         pushOperator();
-        pushKeyArray();
         topDisplay();
-        currentResult();
-        //console.log(operator)
-        console.log(keyString)
-        console.log(keyArray)
+                                       
+        console.log("operator: " + operator)
+        console.log("operator: " + keyString + " string")          
+        console.log("operator: " + keyArray + " array")          
+        console.log("operator: " + runningTotal + " running total") 
         keyString = "";
+        
+        // if (keyArray.length > 1 ) {
+        //     keyArray.splice(0, 2, runningTotal)
+        // } else
+        // return  
     });
 });
 
-function currentResult() {
+function currentResult(runningTotal) {
     if (keyArray.length > 1) {
         
         let zeroIndexToNumber = Number(keyArray[0])
         let firstIndexToNumber = Number(keyArray[1])
-
-        let result = operate(zeroIndexToNumber, firstIndexToNumber, operatorObj[operator])
-
+        
+        runningTotal = operate(zeroIndexToNumber, firstIndexToNumber, operatorObj[operator])
+        
         let displayCurrentResult = document.querySelector(`.number-display`)
-        displayCurrentResult.textContent = result;
+        displayCurrentResult.textContent = parseFloat(runningTotal.toFixed(13));
+                                             
+        console.log("currentResult: " + runningTotal + " running total") 
+        if (keyArray.length > 1 ) {
+            keyArray.splice(0, 2, runningTotal)
+        } else
+        return 
     } 
 }
 
+function runningArray(runningTotal) { 
+    //console.log(runningTotal) 
+}
 
 equalsKey.addEventListener(`click`, function (e) {
     // if (display = typeof Number) {
@@ -85,8 +108,7 @@ equalsKey.addEventListener(`click`, function (e) {
     let result = operate(zeroIndexToNumber, firstIndexToNumber, operatorObj[operator])
 
     let displayEqualResult = document.querySelector(`.number-display`)
-    displayEqualResult.textContent = result;
-    
+    displayEqualResult.textContent = parseFloat(result.toPrecision(13));   
 });
 
 
@@ -107,10 +129,10 @@ function pushKeyArray() {
 }
 
 
-function displayNumKey(keyStringChoice) {
-
+function displayNumKey() {
     let numDisplay = document.querySelector(`.number-display`);
-    numDisplay.textContent = keyStringChoice;
+    numDisplay.textContent = keyString;
+    //console.log(keyStringChoice)
 }
 
 
@@ -118,6 +140,7 @@ function topDisplay() {
     let operatorDisplay = document.querySelector(`.operator-display`);
     let content = document.createElement(`div`);
     content.textContent = display;
+    content.classList.add(`top-display-numbers`)
     operatorDisplay.appendChild(content)
 }
 
